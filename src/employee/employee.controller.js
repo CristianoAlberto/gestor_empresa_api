4570dea1900b12aec8886398b866fb42453303d9
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-05-17 12:05:37 +0100

    feat: employee controller feito

--- src/employee/employee.controller.js
+++ src/employee/employee.controller.js
@@ -6,7 +6,35 @@ class EmployeeController {
             const dataUsers = await employeeService.getAllUsers();
             res.status(200).json(dataUsers)
         } catch (error) {
-            res.status(500).json({ error: 'Erros ao buscar funcionários.' })
+            res.status(500).json({ message: error.message })
+        }
+    }
+
+    async createEmployee(req, res) {
+        try {
+            const dataEmployee = req.body
+            const data = await employeeService.createEmployee(dataEmployee)
+            res.status(201).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
+
+    async updateEmployee(req, res) {
+        try {
+            const data = await employeeService.updateEmployee({ id: req.params.id, ...req.body })
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
+
+    async deleteEmployee(req, res) {
+        try {
+            const data = await employeeService.deleteEmployee({ id: req.params.id })
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
         }
     }
 }
