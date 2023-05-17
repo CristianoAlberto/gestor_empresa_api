56212cb8b270d70e21eb55ac8bc6cc9e05329d5e
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-05-17 11:36:30 +0100

    feat: employee router feito

--- src/employee/employee.router.js
+++ src/employee/employee.router.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const employeeController = require('./employee.controller')
 
-router.get('./userEmployee', employeeController.getAllUsers)
+router.get('./userEmployeeGet', employeeController.getAllUsers)
+router.post('./userEmployeeCreate', employeeController.getAllUsers)
+router.put('./userEmployeeUpdate/:id', employeeController.getAllUsers)
+router.delete('./userEmployeeDelete/:id', employeeController.getAllUsers)
+
 
 module.exports = router
