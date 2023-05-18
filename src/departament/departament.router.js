bf968fd428cc6a2102ce5d2d870b721c992fb0fb
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-05-18 16:36:52 +0100

    feat: Updates

--- src/departament/departament.router.js
+++ src/departament/departament.router.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const departamentController = require('./departament.controller')
 
 router.get('/departamentGet', departamentController.getAllDepartament)
-router.post('/departamentGet', departamentController.createDepartament)
-router.put('/departamentGet', departamentController.updateDepartament)
-router.delete('/departamentGet', departamentController.deleteDepartament)
+router.post('/departamentCreate', departamentController.createDepartament)
+router.put('/departamentUpdate/:id', departamentController.updateDepartament)
+router.delete('/departamentDelete/:id', departamentController.deleteDepartament)
 
 module.exports = router
